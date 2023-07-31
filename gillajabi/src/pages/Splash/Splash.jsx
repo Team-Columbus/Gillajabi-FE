23de66f6f49d8ac805ea92ade2c4800a117f1d76
OhSeChan <seb_s19@naver.com>
2023-08-01 05:48:13 +0900

    :sparkles: Feat : Splash 페이지 추가

--- gillajabi/src/pages/Splash/Splash.jsx
+++ gillajabi/src/pages/Splash/Splash.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Splashpage = () => {
+const Splash = () => {
+  
   return (
-    <div>
-      스플래시 스크린 페이지입니다.
+    <div className='splash'>
+      <div className='splash-text'>
+        <p>손끝으로 배우는</p>
+        <p>새로운 세상</p>
+        <p>내 손안의 키오스크</p>
+        <h2>길라잡이</h2>
+      </div>
+      <div className='splash-imgwrapper'>
+        <img src='' alt='splash-img'/>
+      </div>
+      <div className='splash-footer'>
+        <p>@ Team-Colombus</p>
+        <p>Designed by Freepik</p>
+      </div>
     </div>
   );
 };
 
-export default Splashpage;
+export default Splash;
