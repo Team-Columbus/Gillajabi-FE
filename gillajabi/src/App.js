184af3ab5613dd8175f83498c3e662d1f4a20996
OhSeChan <seb_s19@naver.com>
2023-08-01 20:47:03 +0900

    :sparkles: Feat : Splash screen 기능 추가
    - Splash 페이지의 라우팅을 "/" 로 수정
    - 사용자의 방문 여부를 sessionStorage에 저장
    - 방문 기록이 있다면 메인 페이지, 없다면 Splash 페이지를 보여주도록 구현
    - Splash screen은 4초동안 보여지도록 구현

--- gillajabi/src/App.js
+++ gillajabi/src/App.js
@@ -1,23 +1,46 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Main, Splash, Signup, Login, Mypage, Category} from './pages'
 import './App.css'
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const visited = sessionStorage.getItem('visited');
+
+    const hideSplashScreen = () => {
+      setShowSplash(false);
+      sessionStorage.setItem('visited', 'true');
+    };
+
+    if (visited) {
+      setShowSplash(false);
+    } else {
+      const timer = setTimeout(hideSplashScreen, 4000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <div className='app'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/splash" element={<Splash />} />
-          <Route path="/category" element={<Category />} />
+          {showSplash ? <Route path="/" element={<Splash />} />
+            : (
+            <>
+              <Route path="/" element={<Main />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/category" element={<Category />} />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
