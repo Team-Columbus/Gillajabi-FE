cfeea087df0547f5a8c80830ef8506a5a8dc56bc
0Jaemin0 <jaemin2167@naver.com>
2023-08-06 23:21:52 +0900

    :sparkles: Mypage 페이지 Navbar 컴포넌트 추가
    - 다른 페이지, 로그인 상태로 설정 후 구독 상태에 따라 다른 Navbar
      컴포넌트 추가
    - 현재 구독 정보를 정적으로 설정 후에 동적으로 처리

--- gillajabi/src/pages/Mypage/Mypage.jsx
+++ gillajabi/src/pages/Mypage/Mypage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Navbar from '../../components/Navbar'
+import Button from '../../components/Button'
+import '../../styles/pages/Mypage.css'
+import { faFaceSmile } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+//props로 회원 정보 : 이름, 생년월일, 구독 상태, 구독 시작일, 구독 만료일
 const Mypage = () => {
+  const subscribeStatus = 1; // 1 : 구독 중, 0 : 구독 안함
+
   return (
-    <div>
+    <div className='mypage'>
+      <Navbar
+        pageStatus={0}
+        loginStatus={1}
+        subscribeStatus={subscribeStatus}
+      />
       마이 페이지입니다.
     </div>
   );
