230ca32da96b46ec4dd7837ddad850ef4963b6f2
0Jaemin0 <jaemin2167@naver.com>
2023-08-06 23:42:16 +0900

    :sparkles: 사용자 정보 추가
    - 사용자 이름 및 생년월일 추가
    - 현재 사용자 정보를 정적으로 설정 추후 동적으로 수정 예정
    - Button 컴포넌트를 사용해 수정하기 버튼 추가
    - 수정 페이지 생성 후 수정하기 버튼 동작 추가 예정
    - fontawesome Icon으로 웃는 얼굴 사용
    - 사용자 정보 배치를 위한 CSS 파일 추가

--- gillajabi/src/pages/Mypage/Mypage.jsx
+++ gillajabi/src/pages/Mypage/Mypage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Navbar from '../../components/Navbar'
 import Button from '../../components/Button'
 import '../../styles/pages/Mypage.css'
-import { faFaceSmile } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 //props로 회원 정보 : 이름, 생년월일, 구독 상태, 구독 시작일, 구독 만료일
 const Mypage = () => {
   const subscribeStatus = 1; // 1 : 구독 중, 0 : 구독 안함
+
+  const Edit = () => {
+    //수정 페이지로 이동
+  };
 
   return (
     <div className='mypage'>
@@ -16,7 +20,26 @@ const Mypage = () => {
         loginStatus={1}
         subscribeStatus={subscribeStatus}
       />
-      마이 페이지입니다.
+      <div className='mypage-information'>
+        <div className='mypage-myInformation'>
+          홍길동님의 정보
+          <Button
+            children={'수정하기'}
+            styleType={'Large_Orange'}
+            onClick={Edit}
+            disabled={false}
+          />
+        </div>
+        <div className='mypage-face'>
+          <FontAwesomeIcon icon={faFaceSmile} />
+        </div>
+        <div className='mypage-name'>
+          이름 <span className='mypage-priavacy'>홍길동</span>
+        </div>
+        <div className='mypage-birth'>
+          생년월일<span className='mypage-priavacy'>2001-02-12</span>
+        </div>
+      </div>
     </div>
   );
 };
