f49e50220f2a8b49b86be21e6c176f6f438fe946
OhSeChan <seb_s19@naver.com>
2023-08-07 14:54:47 +0900

    :sparkles: welcome 컴포넌트 구현
    - 회원가입 성공 시 사용자에게 보여질 welcome 컴포넌트 구현
    - 사용자 이름을 표기하고 시작하기 버튼을 누르면 로그인 화면으로
      이동하도록 구현

--- gillajabi/src/pages/Signup/components/Welcome.jsx
+++ gillajabi/src/pages/Signup/components/Welcome.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from '../../../components/Button';
+import { useNavigate } from 'react-router-dom';
+import welcomeImg from '../../../assets/Firecracker.png'
+import { useUserStore } from '../../../stores/userStore';
 
 const Welcome = () => {
+  const navigate = useNavigate();
+  const {user} = useUserStore();
+
+  const startButton = () => {
+    navigate('/login');
+  };
+
   return (
-    <div>
-      회원가입 완료 시 보여줄 컴포넌트
+    <div className='signup-welcome'>
+      <div className='signup-welcome-title'>
+        <h2>환영합니다!</h2>
+        <h2>{user.name}님</h2>
+      </div>
+      <div className='signup-welcome-imgwrapper'>
+        <img src={welcomeImg} alt='welcome-img'/>
+      </div>
+      <div className='signup-welcome-button'>
+        <Button styleType='Large_Orange' onClick={startButton}>
+          시작하기
+        </Button>
+      </div>
     </div>
   );
 };
