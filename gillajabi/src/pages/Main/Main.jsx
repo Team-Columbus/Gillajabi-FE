dcc524d1f03e3289482615d2fed2ce5c132aad8b
0Jaemin0 <jaemin2167@naver.com>
2023-08-09 15:41:24 +0900

    :sparkles: 메인 페이지 구현
    - 카테고리를 보여주는 메인 페이지 구현
    - 공통 Navbar 컴포넌트 사용
    - 공통 Sentence 컴포넌트 사용 => 정적으로 문장 선언
    - 정적 배열로 카테고리에 대한 정보 저장
    - 생성한 카테고리 배열을 map 함수를 사용해 공통 Option 컴포넌트에 전달
      후 카테고리 출력

--- gillajabi/src/pages/Main/Main.jsx
+++ gillajabi/src/pages/Main/Main.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import Sentence from '../../components/Sentence';
+import Option from '../../components/Option';
+import { faBurger, faClapperboard, faMugSaucer, faTrain, faSackDollar } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
+import '../../styles/pages/Main.css'
 
 const Main = () => {
+  const loginStatus = 0; // 1 : 로그인 상태, 0 : 로그아웃 상태 => 로그인 페이지에서 전달 받기
+  const subscribeStatus = 0; // 1 : 구독 중, 0 : 구독 안함 => 로그인 상태일때 구독 여부 받기
+  const pageStatus = 1; // 1 : 메인 페이지, 0 : 다른 페이지
+  
+  const mainSentence = '어떤 분야의 학습을 원하시나요?';
+  const subSentence = '원하시는 분야를 눌러주세요.';
+
+
+  const categoryList = [
+    {
+      categoryIcon: faBurger,
+      categoryName: '패스트푸드',
+      className: 'category',
+      id : 'fastFood',
+    },
+    {
+      categoryIcon: faClapperboard,
+      categoryName: '영화관',
+      className: 'category',
+      id : 'Cinema',
+    },
+    {
+      categoryIcon: faMugSaucer,
+      categoryName: '카페',
+      className: 'category',
+      id : 'Cafe',
+    },
+    {
+      categoryIcon: faTrain,
+      categoryName: '교통',
+      className: 'category',
+      id : 'Traffic',
+    },
+    {
+      categoryIcon: faAddressCard,
+      categoryName: '무인민원발급기',
+      className: 'category',
+      id : 'Machine',
+    },
+    {
+      categoryIcon: faSackDollar,
+      categoryName: 'ATM',
+      className: 'category',
+      id : 'Atm',
+    },
+  ];
+
   return (
-    <div>
-      메인 페이지입니다.
+    <div className='main'> 
+      <Navbar
+        pageStatus={pageStatus}
+        loginStatus={loginStatus}
+        subscribeStatus={subscribeStatus}
+      />
+      <Sentence mainSentence={mainSentence} subSentence={subSentence} />
+      <div className='main-category'>
+        {categoryList.map((info, index) => (
+          <Option
+            className={info.className}
+            iconName={info.categoryIcon}
+            Text={info.categoryName}
+            id={info.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
