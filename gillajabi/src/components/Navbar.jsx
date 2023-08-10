f7744cdef626420d41181056aed02ae8f2c809aa
0Jaemin0 <jaemin2167@naver.com>
2023-08-10 19:37:40 +0900

    :sparkles: 공통 Navbar 컴포넌트 이미지 사용
    - 기존의 상단에 Icon 하단에 Text 형태의 Option 컴포넌트에서 하나의
      이미지만을 사용하는 방법으로 수정
    - Navbar에 필요한 이미지 import 하여 사용

--- gillajabi/src/components/Navbar.jsx
+++ gillajabi/src/components/Navbar.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
 import '../styles/components/Navbar.css'
-import { faUser, faHouse, faDownload, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faCircleQuestion, faCircleLeft } from '@fortawesome/free-regular-svg-icons';
-import Option from './Option.jsx';
-
-/**
- * Navbar 컴포넌트는 헤더 바를 구성하는 React 컴포넌트입니다.
- * @param {number} props.loginStatus - 로그인 상태를 나타내는 숫자 값 (0: 비로그인 상태, 1: 로그인 상태).
- * @param {number} props.pageStatus - 페이지 상태를 나타내는 숫자 값 (0: 처음화면, 1: 다른화면).
- * @param {number} props.subscribeStatus - 구독 상태를 나타내는 숫자 값 (0: 구독하지 않음, 1: 구독 중).
- * @returns {JSX.Element} 헤더 바를 나타내는 JSX 요소
- */
+import {Back, Glasses, Home, Load, Login, myInfo, Solve} from '../assets/'
 
 const Navbar = (props) => {
-  const loginText = props.loginStatus === 1 ? '내 정보' : '로그인 하기';
+  const loginText = props.loginStatus === 1 ?  myInfo : Login ;
 
   return (
     <div className='navbar'>
       {props.pageStatus === 1 ? (
-        <Option iconName={faUser} Text={loginText} />
+        <div className='navbar-img'>
+          <img src= {loginText} />
+        </div>
       ) : (
-        <Option iconName={faCircleLeft} Text={'이전화면'} />
+        <div className='navbar-img'>
+          <img src= {Back} />
+        </div>
       )}
+
       {props.subscribeStatus === 1 ? (
-        <Option iconName={faCircleQuestion} Text={'오늘의 문제'} />
+        <div className='navbar-img'>
+          <img src= {Solve} />
+        </div>
       ) : null}
 
       {props.pageStatus === 0 ? (
-        <Option iconName={faHouse} Text={'처음화면'} />
+        <div className='navbar-img'>
+          <img src= {Home} />
+        </div>
       ) : null}
 
       {props.subscribeStatus === 1 ? (
-        <Option iconName={faDownload} Text={'불러오기'} />
+        <div className='navbar-img'>
+          <img src= {Load} />
+        </div>
       ) : null}
 
-      <Option iconName={faMagnifyingGlass} Text={'돋보기'} />
+      <div className='navbar-img'>
+        <img src= {Glasses} />
+      </div>
     </div>
   );
 };
