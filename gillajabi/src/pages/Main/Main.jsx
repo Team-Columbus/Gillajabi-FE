3af2c8f6c09b55b5549d7e714d0a49cb8394f3c3
0Jaemin0 <jaemin2167@naver.com>
2023-08-10 19:55:14 +0900

    :sparkles: 메인 페이지 카테고리 변경
    - 기존의 상단에 Icon 하단에 Text 형태의 Option 컴포넌트에서 이미지만을
      사용하는 방법으로 수정
    - 메인 페이지에 필요한 카테고리 이미지 사용
    - 메인 페이지에 사용되는 정적 이미지 배열 생성 후 map 함수 사용

--- gillajabi/src/pages/Main/Main.jsx
+++ gillajabi/src/pages/Main/Main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Sentence from '../../components/Sentence';
-import Option from '../../components/Option';
-import { faBurger, faClapperboard, faMugSaucer, faTrain, faSackDollar } from '@fortawesome/free-solid-svg-icons';
-import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
 import '../../styles/pages/Main.css'
+import {Atm, Cafe, Cinema, fastFood, Machine, Traffic} from '../../assets/'
 
 const Main = () => {
   const loginStatus = 0; // 1 : 로그인 상태, 0 : 로그아웃 상태 => 로그인 페이지에서 전달 받기
@@ -14,45 +12,7 @@ const Main = () => {
   const mainSentence = '어떤 분야의 학습을 원하시나요?';
   const subSentence = '원하시는 분야를 눌러주세요.';
 
-
-  const categoryList = [
-    {
-      categoryIcon: faBurger,
-      categoryName: '패스트푸드',
-      className: 'category',
-      id : 'fastFood',
-    },
-    {
-      categoryIcon: faClapperboard,
-      categoryName: '영화관',
-      className: 'category',
-      id : 'Cinema',
-    },
-    {
-      categoryIcon: faMugSaucer,
-      categoryName: '카페',
-      className: 'category',
-      id : 'Cafe',
-    },
-    {
-      categoryIcon: faTrain,
-      categoryName: '교통',
-      className: 'category',
-      id : 'Traffic',
-    },
-    {
-      categoryIcon: faAddressCard,
-      categoryName: '무인민원발급기',
-      className: 'category',
-      id : 'Machine',
-    },
-    {
-      categoryIcon: faSackDollar,
-      categoryName: 'ATM',
-      className: 'category',
-      id : 'Atm',
-    },
-  ];
+  const categoryList = [ fastFood, Cinema, Cafe, Traffic, Machine, Atm];
 
   return (
     <div className='main'> 
@@ -64,12 +24,9 @@ const Main = () => {
       <Sentence mainSentence={mainSentence} subSentence={subSentence} />
       <div className='main-category'>
         {categoryList.map((info, index) => (
-          <Option
-            className={info.className}
-            iconName={info.categoryIcon}
-            Text={info.categoryName}
-            id={info.id}
-          />
+          <div className='main-categoryImg'>
+            <img src = {info}/>
+          </div>
         ))}
       </div>
     </div>
