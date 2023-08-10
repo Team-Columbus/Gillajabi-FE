86e6463e49d2c56467262ffee626dba02dee2732
0Jaemin0 <jaemin2167@naver.com>
2023-08-10 19:26:42 +0900

    :bento: Navbar 컴포넌트 이미지 추가
    - Navbar 컴포넌트에 사용되는 이미지 추가
    - 이미지를 모듈로 내보내기 위한 index.js 파일 추가

---
+++ gillajabi/src/assets/index.js
@@ -0,0 +1,7 @@
+export const Back = require('./Navbar/Back.png');
+export const Glasses = require('./Navbar/Glasses.png');
+export const Home = require('./Navbar/Home.png');
+export const Load = require('./Navbar/Load.png');
+export const Login = require('./Navbar/Login.png');
+export const myInfo = require('./Navbar/myInfo.png');
+export const Solve= require('./Navbar/Solve.png');
