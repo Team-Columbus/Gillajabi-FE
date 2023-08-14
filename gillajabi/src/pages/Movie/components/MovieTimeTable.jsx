f83a8238208f4d044b6177a602a0901787c5f8e7
OhSeChan <seb_s19@naver.com>
2023-08-14 19:02:46 +0900

    :sparkles: MovieTimeTalbe 컴포넌트 구현
    - 영화 상영 시간표를 서버에서 영화 데이터를 가져와 useState로 상태 관리
    - 영화 리스트를 map 함수를 이용하여 MovieTimeTableItem 컴포넌트에
      데이터를 전달하여 화면에 표시

--- gillajabi/src/pages/Movie/components/MovieTimeTable.jsx
+++ gillajabi/src/pages/Movie/components/MovieTimeTable.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieTimeTableItem from './MovieTimeTable/MovieTimeTableItem';
 
 const MovieTimeTable = () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    getMovieList();
+  },[])
+
+  const getMovieList = async (e) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API}/api/theaters/CGVmovie_list/`);
+      setMovieList(response.data);
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
-    <div>
-      전체 상영 시간표입니다.
+    <div className='movie-timetable'>
+      {movieList.map((item,idx)=>(
+        <MovieTimeTableItem item={item} key={idx}/>
+      ))}
     </div>
   );
 };
