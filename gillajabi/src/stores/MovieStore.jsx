f8c25d49796ac7b4aa355549dd097fb68899f43c
OhSeChan <seb_s19@naver.com>
2023-08-15 21:35:51 +0900

    :sparkles: MovieStore에 선택 시간대 좌석 정보 추가
    - 사용자가 선택한 영화의 좌석 정보를 컴포넌트 간 공유를 위해
      selectSeatNum과 setSelectSeatNum 추가

--- gillajabi/src/stores/MovieStore.jsx
+++ gillajabi/src/stores/MovieStore.jsx
@@ -2,10 +2,13 @@ import { create } from 'zustand';
 
 export const useMovieStore = create((set, get) => ({
   userCount : null,
-  
   selectedMovie : null,
-  
-  setUserCount: (count) => set({ userCount: count }),
+  selectSeatNum : null,
+
+  setUserCount: (count) => set({ userCount: count }),  
   
   setSelectedMovie: (title) => set({ selectedMovie: title }),
+
+  setSelectSeatNum: (seatNum) => set({ selectSeatNum: seatNum }),
+
 }));
