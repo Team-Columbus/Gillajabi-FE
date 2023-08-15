35c98a6d93683977239ec13566c78b251c791593
0Jaemin0 <jaemin2167@naver.com>
2023-08-15 17:09:38 +0900

    :sparkles: BusSelect 컴포넌트 구현
    - 날짜, 출발지, 목적지를 선택하는 BusSelect 컴포넌트 구현
    - BusHeader 컴포넌트 사용
    - 선택 사항 클릭 시 선택 메뉴가 나오는 컴포넌트 추후 구현

---
+++ gillajabi/src/pages/Bus/components/BusSelect.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useBusContext } from '../context/BusContext';
+import BusHeader from './BusHeader';
+import Calender from '../../../assets/Bus/BusSelect_Calender.png'
+import '../../../styles/pages/Bus/BusSelect.css'
+
+const BusSelect = () => {
+  return (
+    <div className='busselect'>
+      <BusHeader/>
+      <div className='busselect-title'>
+        도착지를 선택하세요
+      </div>
+      <div className='busselect-detail'>
+        <div className='detail-depart'>
+          <img src={Calender} /><span>출발일</span>
+        </div>
+        <div className='detail-date'>
+          <div className='detail-date-back'>
+            {'〈'} 이전날
+          </div>
+          <div className='detail-date-middle'>
+            2023-08-15 화요일
+          </div>
+          <div className='detail-date-next'>
+            다음날 {'〉'}
+          </div>
+        </div>
+        <div className='detail-selectBox'>
+        </div>  
+        <div className='detail-excuse'>
+          <div className='excuse-line'>
+            <span class="excuse-emphasis">코로나19</span>&nbsp;로 인하여 일부 노선의
+          </div>
+          <div className='excuse-line'>
+            운행 시간이 변경 및 조정될 수 있습니다.
+          </div>
+          <div className='excuse-line'>
+            양해 드립니다.
+          </div>
+        </div>
+        <div className='detail-caution'>
+          <div className='caution-line'>
+            출발 <span className='caution-emphasis'>5분</span>&nbsp;전부터는 발권되지 않습니다.
+          </div>
+          <div className='caution-line'>
+            다음 출발 차량을 이용해주세요.
+          </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusSelect;
