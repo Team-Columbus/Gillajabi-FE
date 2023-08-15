018c560081a3d7126d32a44f4c3fd279ba8a3b47
OhSeChan <seb_s19@naver.com>
2023-08-15 21:46:59 +0900

    :sparkles: MovieSelectSeat 영화 좌석 표시 기능 구현
    - 사용자가 좌석을 선택할 수 있도록 이미 예매된 좌석과 남은 좌석을 구분할
      수 있도록 구현
    - useMovieStore를 사용하여 영화 예매 정보를 가져오고  예매된 좌석을
      임의로 생성하여 비활성화
    - 예매된 좌석은 selectedSeatNum으로 상태 관리하며 변경 시 useEffect로
      선택한 좌석을 갱신할 수 있도록 구현

--- gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
+++ gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useMovieStore } from '../../../stores/MovieStore';
+import Button from '../../../components/Button';
 
 const MovieSelectSeat = () => {
+
+  const { userCount, selectSeatNum } = useMovieStore();
+
+  const selectedSeatNum = (40 - selectSeatNum);
+  
+  const selectType = ['A', 'B', 'C', 'D'];
+
+  const [selectedSeats, setSelectedSeats] = useState([]);
+
+  useEffect(() => {
+    updateSelectedSeats();
+  }, [selectedSeatNum]);
+
+  const updateSelectedSeats = () => {
+    const randSeats = [];
+    while (randSeats.length < selectedSeatNum) {
+      const randomSeatIdx = Math.floor(Math.random() * 40);
+      if (!randSeats.includes(randomSeatIdx)) {
+        randSeats.push(randomSeatIdx);
+      }
+    }
+    setSelectedSeats(randSeats);
+  };
+
   return (
     <div className='movie-selectseat'>
-      영화 좌석 선택 화면입니다.
+      <div className='movie-selectseat-header'>
+        <div id='header-selected'>
+          <p>일반</p>
+          <p>1</p>
+        </div>
+        <div id='header-payment'>
+          <p>총 결제금액</p>
+          <p>0원</p>
+        </div>
+      </div>
+      <div className='movie-selectseat-seat'>
+        <p>SCREEN</p>
+        {selectType.map((type, typeIndex) => (
+          <div key={type} className='movie-selectseat-seat-group'>
+            <p>{`${type}`}</p>
+            <div className='button-group'>
+              {Array.from({ length: 10 }, (_, index) => {
+                const seatNumber = typeIndex * 10 + index + 1;
+                const isDisabled = selectedSeats.includes(seatNumber - 1);
+                return (
+                  <button 
+                    key={index} 
+                    disabled={isDisabled}
+                    className={isDisabled ? 'disabled-button' : 'enabled-button'}>
+                    {isDisabled ? '': `${type}${String(seatNumber).padStart(2, '0')}`}
+                  </button>
+                );
+              })}
+            </div>
+            <p>{`${type}`}</p>
+          </div>
+        ))}
+      </div>
+      <Button>
+        결제할래요
+      </Button>
     </div>
   );
 };
