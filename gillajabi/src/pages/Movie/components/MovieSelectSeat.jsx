debae0d43607fb910e56a646bfcf6a4b3a4e3a6e
OhSeChan <seb_s19@naver.com>
2023-08-15 21:53:35 +0900

    :lipstick: MoveSelectSeat 스타일 추가
    - 예매된 좌석과 남은 좌석의 구분할 수 있도록 스타일 구현

--- gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
+++ gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useMovieStore } from '../../../stores/MovieStore';
 import Button from '../../../components/Button';
+import '../../../styles/pages/Movie/MovieSelectSeat.css';
 
 const MovieSelectSeat = () => {
 
