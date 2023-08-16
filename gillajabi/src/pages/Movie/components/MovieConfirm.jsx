6138a3dfad3057a69d3443c623c7015aa1b3d5b6
OhSeChan <seb_s19@naver.com>
2023-08-17 05:55:59 +0900

    :sparkles: MovieConfirm 컴포넌트 기능 구현
    - 사용자 선택 영화의 id 값을 통해 영화 정보를 가져와 moveInfo로 상태관리
    - 날짜 정보를 가져오기 위한 getDateInfo 함수 구현, 날짜 정보를
      dateInfo로 상태관리
    - MovieStore에서 결제 금액을 가져와 화면에 표시

--- gillajabi/src/pages/Movie/components/MovieConfirm.jsx
+++ gillajabi/src/pages/Movie/components/MovieConfirm.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useMovieStore } from '../../../stores/MovieStore';
+import Button from '../../../components/Button';
 
 const MovieConfirm = () => {
+
+  const { moviePayment, selectedMovieInfo } = useMovieStore();
+  const [movieInfo, setMovieInfo] = useState();
+  const [dateInfo, setDateInfo] = useState();
+
+  useEffect(()=>{
+    getMovieInfo(selectedMovieInfo.id);
+    getDateInfo();
+  },[])
+  
+  const getMovieInfo = async (id) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API}/api/theaters/CGVmovie_detail/${id}/`);
+      setMovieInfo(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const getDateInfo = () => {
+    const now = new Date();
+    const dateOptions = {
+      year : 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+    };
+    const formattedDate = now.toLocaleString(undefined, dateOptions);
+    setDateInfo(formattedDate);
+  };
+
   return (
-    <div>
-      예매 정보 확인 컴포넌트입니다.
+    <div className='movie-confirm'>
+      <div className='movie-confirm-header'>
+        <div id='header-payment'>
+          <p>총 결제금액</p>
+          <p>{moviePayment}원</p>
+        </div>
+      </div>
+      <div className='movie-confirm-content'>
+        <p>예매 내역을 확인하세요</p>
+        <div className='movie-confirm-content-line'>
+          <p>예매정보</p>
+          <div id='content-hrLine'/>
+        </div>
+      </div>
+      <Button styleType={'Movie_Red'}>
+        결제할래요
+      </Button>
     </div>
   );
 };
