9b2ffd21766ca9e07b90c48b40ba00109579e0cb
OhSeChan <seb_s19@naver.com>
2023-08-16 20:52:48 +0900

    :sparkles: MovieSelectSeatGroup 컴포넌트 추가
    - MovieSelectSeat 컴포넌트에서 사용자 좌석 선택에 대한 부분을 분리하여 MovieSelectSeatGroup 컴포넌트를 추가
    - MovieSelectSeat 컴포넌트에서 사용자 선택 좌석 정보 userSelectSeats를
      prop으로 받아 사용자의 선택에 따라 갱신하도록 구현
    - A01 ~ D10까지의 좌석을 표현하기 위해 번호를 구분하고 padStart 함수를
      사용하여 형식에 맞춰 포맷팅하는 함수를 추가
    - 포맷팅 함수의 추가에 따라 기존 로직에서 숫자로 좌석을 구분하던
      방식에서 포맷팅된 좌석 번호로 구분하도록 변경

--- gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
+++ gillajabi/src/pages/Movie/components/MovieSelectSeat.jsx
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useMovieStore } from '../../../stores/MovieStore';
+import React, { useState } from 'react';
 import MovieSelectSeatHeader from './MovieSelectSeat/MovieSelectSeatHeader';
+import MovieSelectSeatGroup from './MovieSelectSeat/MovieSelectSeatGroup';
 import Button from '../../../components/Button';
 import '../../../styles/pages/Movie/MovieSelectSeat.css';
 
 const MovieSelectSeat = () => {
 
-  const { userCount, selectSeatNum } = useMovieStore();
-
-  const selectedSeatNum = (40 - selectSeatNum);
-  
-  const selectType = ['A', 'B', 'C', 'D'];
   const [totalPayment, setTotalPayment] = useState(0);
-  const [selectedSeats, setSelectedSeats] = useState([]);
-
-  useEffect(() => {
-    updateSelectedSeats();
-  }, [selectedSeatNum]);
-
-  const updateSelectedSeats = () => {
-    const randSeats = [];
-    while (randSeats.length < selectedSeatNum) {
-      const randomSeatIdx = Math.floor(Math.random() * 40);
-      if (!randSeats.includes(randomSeatIdx)) {
-        randSeats.push(randomSeatIdx);
-      }
-    }
-    setSelectedSeats(randSeats);
-  };
+  const [userSelectSeats, setUserSelectSeats] = useState([]);
 
   return (
     <div className='movie-selectseat'>
@@ -36,26 +16,9 @@ const MovieSelectSeat = () => {
         setTotalPayment={setTotalPayment}/>
       <div className='movie-selectseat-seat'>
         <p>SCREEN</p>
-        {selectType.map((type, typeIndex) => (
-          <div key={type} className='movie-selectseat-seat-group'>
-            <p>{`${type}`}</p>
-            <div className='button-group'>
-              {Array.from({ length: 10 }, (_, index) => {
-                const seatNumber = typeIndex * 10 + index + 1;
-                const isDisabled = selectedSeats.includes(seatNumber - 1);
-                return (
-                  <button 
-                    key={index} 
-                    disabled={isDisabled}
-                    className={isDisabled ? 'disabled-button' : 'enabled-button'}>
-                    {isDisabled ? '': `${type}${String(seatNumber).padStart(2, '0')}`}
-                  </button>
-                );
-              })}
-            </div>
-            <p>{`${type}`}</p>
-          </div>
-        ))}
+        <MovieSelectSeatGroup 
+          userSelectSeats={userSelectSeats} 
+          setUserSelectSeats ={setUserSelectSeats}/>
       </div>
       <Button>
         결제할래요
---
+++ gillajabi/src/pages/Movie/components/MovieSelectSeat/MovieSelectSeatGroup.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { useMovieStore } from '../../../../stores/MovieStore';
+
+const MovieSelectSeatGroup = (props) => {
+
+  const { userSelectSeats, setUserSelectSeats} = props;
+  const { userCount, selectSeatNum } = useMovieStore();
+
+  const [selectedSeats, setSelectedSeats] = useState([]);
+
+  const selectType = ['A', 'B', 'C', 'D'];
+  const selectedSeatNum = (40 - selectSeatNum);
+  const totalCount = Object.values(userCount).reduce((acc, count) => acc + count, 0);
+
+  useEffect(() => {
+    updateSelectedSeats();
+  },[]);
+
+  const updateSelectedSeats = () => {
+    const randSeats = [];
+    while (randSeats.length < selectedSeatNum) {
+      const randSeatIdx = Math.floor(Math.random() * 40);
+      const seatNumber = formatSeatNum(randSeatIdx);
+      if (!randSeats.includes(seatNumber)) {
+        randSeats.push(seatNumber);
+      }
+    }
+    setSelectedSeats(randSeats);
+  };
+
+  const padNumber = (number) =>{
+    return String(number).padStart(2, '0');
+  }
+
+  const formatSeatNum = (number) => {
+
+    if (number >= 1 && number <= 10) {
+      return 'A' + padNumber(number);
+    } else if (number >= 11 && number <= 20) {
+      return 'B' + padNumber(number - 10);
+    } else if (number >= 21 && number <= 30) {
+      return 'C' + padNumber(number - 20);
+    } else if (number >= 31 && number <= 40) {
+      return 'D' + padNumber(number - 30);
+    } else {
+      return 'Invalid number';
+    }
+  };
+
+  const handleSeatClick = (seatNumber) => {
+    if (userSelectSeats.length < totalCount || userSelectSeats.includes(seatNumber)) {
+      if (!userSelectSeats.includes(seatNumber)) {
+        setUserSelectSeats([...userSelectSeats, seatNumber]);
+      } else {
+        setUserSelectSeats(userSelectSeats.filter((seat) => seat !== seatNumber));
+      }
+    } else{
+      alert(`선택하신 관람인원은 ${totalCount}명입니다. 😯`);
+    }
+  };
+
+  return (
+    <>
+      {selectType.map((type, typeIndex) => (
+        <div key={type} className='movie-selectseat-seat-group'>
+          <p>{`${type}`}</p>
+          <div className='button-group'>
+            {Array.from({ length: 10 }, (_, index) => {
+              const seatNumber = `${type}`+ String(index + 1).padStart(2, '0');
+              const isDisabled = selectedSeats.includes(seatNumber);
+              const isSelected = userSelectSeats.includes(seatNumber);
+              return (
+                <button 
+                  key={index} 
+                  disabled={isDisabled}
+                  className={`${isDisabled ? 'disabled-button' : isSelected ? 'selected-button' : 'enabled-button'}`}
+                  onClick={() => handleSeatClick(seatNumber)}>
+                  {isDisabled ? '' : seatNumber}
+                </button>
+              );
+            })}
+          </div>
+          <p>{`${type}`}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieSelectSeatGroup;
