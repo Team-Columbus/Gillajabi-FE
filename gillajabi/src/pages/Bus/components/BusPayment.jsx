4ea0e66b6b37a161da9af20cc88e2a1c3a5af454
0Jaemin0 <jaemin2167@naver.com>
2023-08-18 06:28:00 +0900

    :sparkles: BusPayment 컴포넌트 구현
    - 결제를 진행하는 BusPayment 컴포넌트 구현
    - 버스 결제 정보 제공 및 결제 진행
    - 두 개의 모달창 컴포넌트 사용
    - useState를 통해 모달창을 열고 닫는 상태 관리

---
+++ gillajabi/src/pages/Bus/components/BusPayment.jsx
@@ -0,0 +1,101 @@
+import {React, useState} from 'react';
+import BusHeader from './BusHeader';
+import Card from '../../../assets/Bus/BusPayment_Card.png';
+import Kiosk from '../../../assets/Bus/BusPayment_Kiosk.png';
+import PaymentModal from './BusPayment/CompletePaymentModal';
+import TicketModal from './BusPayment/TicketPaymentModal';
+import { useBusContext } from '../context/BusContext';
+import { useBusStore } from '../../../stores/BusStore';
+import '../../../styles/pages/Bus/BusPayment.css';
+
+const BusPayment = () => {
+  const [isPaymentModal, setIsPaymentModal] = useState(false);
+  const [isTicketModal, setIsTicketModal] = useState(false)
+  const {handlePage} = useBusContext();
+  const {busFare, busDestination, busDate, butTime, busRate, busCompany } = useBusStore();
+  
+  const busInformation = {
+    버스회사: busCompany,
+    버스등급: busRate,
+    좌석번호: '18, 21',
+    티켓수량: '2 (일반 2매)'
+  }
+
+  const openPaymentModal = () => {
+    setIsPaymentModal(true);
+  };
+
+  const closePaymentModal = () => {
+    setIsPaymentModal(false);
+    openTicketModal();
+  };
+
+  const openTicketModal = () => {
+    setIsTicketModal(true);
+  };
+
+  const closeTicketModal = () => {
+    setIsTicketModal(false);
+    handlePage('BusFinish')
+  };
+
+  return (
+    <div className='buspayment'>
+      <PaymentModal isOpen={isPaymentModal} isClose={closePaymentModal} />
+      <TicketModal isOpen={isTicketModal} isClose={closeTicketModal}/>
+      <BusHeader text='IC카드를 투입구 끝까지 밀어 넣어주세요' />
+      <div className='buspayment-play'>
+        <div className='buspayment-detail'>
+          <div className='buspayment-detail-date'>
+            {busDate}(토) {butTime}
+          </div>
+          <div className='detail-information'>
+            <div className='detail-information-city'>
+
+              <div className='city-depart'>
+                <div className='city-common-circle'>
+                  출발
+                </div>
+                <div className='city-common-text'>
+                  서울경부<span>평균 4시간 소요</span>
+                </div>
+              </div>
+
+              <div className='city-line'>
+              </div>
+
+              <div className='city-arrive'>
+                <div className='city-common-circle'>
+                  도착
+                </div>
+                <div className='city-common-text'>
+                  {busDestination}
+                </div>
+              </div>
+            </div>
+            <div className='detail-information-bus'>
+              {Object.entries(busInformation).map(([key, value]) => (
+                <div className='bus-information'>{key}<span>{value}</span></div>
+              ))}
+            </div>
+          </div>
+          <div className='detail-payment'>
+            결제금액<span>{busFare}원</span>
+          </div>
+          <div className='detail-warning'>
+            ※소요시간은 교통상황에 따라 달라 질 수 있습니다.
+          </div>
+        </div>
+        <div className='buspayment-card'>
+          <img src={Kiosk} alt='Kiosk'></img>
+          <img src={Card} alt='Card'></img>
+        </div>
+        <div className='buspayment-text' onClick={() => openPaymentModal()}>
+          터치하여 카드를 넣어 주세요!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusPayment;
