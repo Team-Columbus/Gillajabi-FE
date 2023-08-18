705e670a95c3e519e4b5d89549ce0b6f017e3415
OhSeChan <seb_s19@naver.com>
2023-08-18 11:43:17 +0900

    :sparkles: Quest 컴포넌트 퀘스트 가져오기 기능 추가
    - 서버로부터 accessToken을 통해 퀘스트 정보에 접근하여 quest state로
      상태 관리
    - optional chainning으로 quest의 category 값에 따라 '영화관', '버스' 인
      경우에 대해 각각 questId를 부여

--- gillajabi/src/pages/Quest/Quest.jsx
+++ gillajabi/src/pages/Quest/Quest.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
+import Button from '../../components/Button';
+import axios from 'axios';
 
 const Quest = () => {
+
+  const [quest, setQuest] = useState(null);
+
+  let questComponent;
+  let questId;
+
+  useEffect(()=>{
+    getQuestInfo();
+  },[]);
+
+  const getQuestInfo = async () =>{
+    const token = localStorage.getItem('accessToken');
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API}/api/quests/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setQuest(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  switch (quest?.content.category) {
+    case '영화관':
+      questId='movie'
+      break;
+    case '버스':
+      questId='transport'
+      break;
+    default:
+      questComponent = undefined;
+      break;
+  };
+
   return (
     <div className='quest'>
       <Navbar/>
-      퀘스트 페이지 입니다.
+      <div className='quest-content'>
+        <div className='quest-content-info'>
+          <div className='quest-content-header'>
+            <p>오늘의 문제</p>
+            <div className='quest-content-header-category' id={questId}>
+              <p>{quest?.content.category}</p>
+            </div>
+          </div>
+          <div className='quest-content-detail'>
+            <div className='quest-content-detail-component'>
+              {questComponent}
+            </div>
+          </div>
+        </div>
+        <Button>
+          수락하기
+        </Button>
+      </div>
     </div>
   );
 };
